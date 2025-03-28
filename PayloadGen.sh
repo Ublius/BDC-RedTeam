c19d1e5e763e91eebe3a12090854875c4bff7f42
Tristen <ublius.developer@gmail.com>
2025-03-28 10:20:57 -0600

    Update PayloadGen.sh

--- PayloadGen.sh
+++ PayloadGen.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Get IP Address
+ip_address=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}')
 
+if [[ -z "$ip_address" ]]; then
+	echo "No IP Address found for eth0"
+else
+	echo "Your IP Address Is: $ip_address"
+fi
